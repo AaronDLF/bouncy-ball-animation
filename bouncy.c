cce0577172460f0c6fc45194486f925074b2240e
AaronDLF <aaron10lopfortich@gmail.com>
2026-01-13 23:52:28 -0500

    Add the trajectory indicator function

--- bouncy.c
+++ bouncy.c
@@ -6,7 +6,9 @@
 #define HEIGHT 600
 #define WHITE_COLOR 0xFFFFFFFF
 #define BLACK_COLOR 0x00000000
+#define BLUE_COLOR 0x4682B4
 #define A_GRAVITY 0.2
+#define TRAJECTORY_SIZE 100
 
 struct Circle {
   double x;
@@ -64,6 +66,35 @@ void step(struct Circle *circle) {
   }
 }
 
+void FillTrajectory(SDL_Surface *psurface, struct Circle circle) {
+  static struct Circle trajectory[TRAJECTORY_SIZE];
+  static int index = 0;
+  trajectory[index] = circle;
+  index = (index + 1) % TRAJECTORY_SIZE;
+
+  for (int i = 0; i < TRAJECTORY_SIZE; i++) {
+    struct Circle traj_circle = trajectory[i];
+    double lower_x = traj_circle.x - traj_circle.radius / 4;
+    double lower_y = traj_circle.y - traj_circle.radius / 4;
+    double higher_x = traj_circle.x + traj_circle.radius / 4;
+    double higher_y = traj_circle.y + traj_circle.radius / 4;
+
+    double radius_squared = (traj_circle.radius / 4) * (traj_circle.radius / 4);
+
+    for (double x = lower_x; x <= higher_x; x++) {
+      for (double y = lower_y; y <= higher_y; y++) {
+        double center_distance_squared =
+            (x - traj_circle.x) * (x - traj_circle.x) +
+            (y - traj_circle.y) * (y - traj_circle.y);
+        if (center_distance_squared < radius_squared) {
+          SDL_Rect pixel = (SDL_Rect){x, y, 1, 1};
+          SDL_FillRect(psurface, &pixel, BLUE_COLOR);
+        }
+      }
+    }
+  }
+}
+
 int main() {
 
   printf("Hello, Bouncy World!\n");
@@ -93,6 +124,7 @@ int main() {
     }
     SDL_FillRect(psurface, &erase_rect, BLACK_COLOR);
     FillCircle(psurface, circle);
+    FillTrajectory(psurface, circle);
     step(&circle);
     SDL_UpdateWindowSurface(pwindow);
     SDL_Delay(20);
